461be87b8410814e56ea589bab50f7c81ad40603
satyag <satyag@satyag-Lenovo-IdeaPad-S300.(none)>
2012-12-17 21:54:28 +0530

    Initial commit

---
+++ sat/truenorth/admin.py
@@ -0,0 +1,77 @@
+from django.contrib import admin
+from models import *
+
+class CourseSectionInline(admin.StackedInline):
+    model = CourseSection
+    extra = 4
+
+class WorksheetInline(admin.StackedInline):
+    model = Worksheet
+    extra = 5
+
+class FullLengthTestInline(admin.StackedInline):
+    model = FullLengthTest
+    extra = 5
+
+class StudentAdmin(admin.ModelAdmin):
+    search_fields = ['full_name']
+    list_filter = ('centre',)
+    save_on_top = True
+
+class TutorAdmin(admin.ModelAdmin):
+    search_fields = ['full_name']
+
+class CourseAdmin(admin.ModelAdmin):
+    save_on_top = True
+    inlines = [CourseSectionInline, FullLengthTestInline]
+
+class CourseModuleAdmin(admin.ModelAdmin):
+    list_filter = ('course',)
+    inlines = [WorksheetInline]
+
+class WorksheetAdmin(admin.ModelAdmin):
+    list_filter = ('module',)
+
+class CentreAdmin(admin.ModelAdmin):
+    pass
+
+class ExamAdmin(admin.ModelAdmin):
+    list_filter = ('course',)
+    
+
+class BatchAdmin(admin.ModelAdmin):
+    list_filter = ('centre', 'module', 'tutor',)
+   
+
+class FullLengthTestAdmin(admin.ModelAdmin):
+    list_filter = ('course',)
+
+class SectionalTestAdmin(admin.ModelAdmin):
+    list_filter = ('section',)
+
+class AttendanceAdmin(admin.ModelAdmin):
+    filter_horizontal = ('students',)
+    list_filter = ('students', 'tutor', 'batch',)
+
+class StudentWorksheetAdmin(admin.ModelAdmin):
+    list_filter = ('student', 'worksheet', 'completed',)
+
+class FeedbackSessionAdmin(admin.ModelAdmin):
+    filter_horizontal = ('students',)
+    list_filter = ('tutor', 'students',)
+
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Guardian)
+admin.site.register(Tutor, TutorAdmin)
+admin.site.register(Centre, CentreAdmin)
+admin.site.register(Course, CourseAdmin)
+admin.site.register(CourseModule, CourseModuleAdmin)
+admin.site.register(Worksheet, WorksheetAdmin)
+admin.site.register(Exam, ExamAdmin)
+admin.site.register(Batch, BatchAdmin)
+admin.site.register(FullLengthTest, FullLengthTestAdmin)
+admin.site.register(SectionalTest, SectionalTestAdmin)
+
+admin.site.register(StudentWorksheet)
+admin.site.register(Attendance, AttendanceAdmin)
+admin.site.register(FeedbackSession, FeedbackSessionAdmin)
---
+++ sat/truenorth/models.py
@@ -0,0 +1,219 @@
+from django.db import models
+from django.contrib.auth.models import User
+from ox.django.fields import DictField
+
+class Student(models.Model):
+    user = models.ForeignKey(User)
+    full_name = models.CharField(max_length=255, blank=True)
+    centre = models.ForeignKey("Centre")
+    address = models.TextField(blank=True)
+    guardian = models.ForeignKey("Guardian", blank=True, null=True)    
+    exams = models.ManyToManyField("Exam", through="StudentExam")
+    courses = models.ManyToManyField("Course", through="StudentCourse")
+    worksheets = models.ManyToManyField("Worksheet", through="StudentWorksheet")
+    full_length_tests = models.ManyToManyField("FullLengthTest", through="StudentFullLengthTest")
+    sectional_tests = models.ManyToManyField("SectionalTest", through="StudentSectionalTest")
+
+    def __unicode__(self):
+        return self.full_name
+
+class Tutor(models.Model):
+    user = models.ForeignKey(User)
+    full_name = models.CharField(max_length=255, blank=True)
+
+    def __unicode__(self):
+        return self.full_name
+
+
+class Guardian(models.Model):
+    '''
+        Parent / guardian
+    '''
+    user = models.ForeignKey(User)
+    full_name = models.CharField(max_length=255, blank=True)    
+
+    def __unicode__(self):
+        return self.full_name
+
+class Centre(models.Model):
+    '''
+        eg. Colaba, Vile Parle
+    '''
+    name = models.CharField(max_length=128, unique=True)
+    address = models.TextField(blank=True)
+
+    def __unicode__(self):
+        return self.name
+
+class Course(models.Model):
+    '''
+        eg. ACT, SAT, IB
+    '''
+    name = models.CharField(max_length=64, unique=True)
+    total_score = models.IntegerField()
+
+    def __unicode__(self):
+        return self.name
+
+
+class CourseModule(models.Model):
+    '''
+        Divide course into modules - eg. English, Math
+    '''
+    name = models.CharField(max_length=64)
+    course = models.ForeignKey(Course)
+
+    def __unicode__(self):
+        return "%s: %s" % (self.course.name, self.name,)
+
+
+class CourseSection(models.Model):
+    name = models.CharField(max_length=128)
+    course = models.ForeignKey(Course)
+    total_marks = models.IntegerField()
+
+    def __unicode__(self):
+        return "%s: %s" % (self.course.name, self.name,)
+
+
+class Worksheet(models.Model):
+    name = models.CharField(max_length=64)
+    module = models.ForeignKey(CourseModule)
+
+    def __unicode__(self):
+        return self.name
+
+
+class StudentWorksheet(models.Model):
+    student = models.ForeignKey(Student)
+    worksheet = models.ForeignKey(Worksheet)
+    completed = models.BooleanField(default=False)
+    notes = models.TextField(blank=True)
+    date = models.DateField()
+
+    def __unicode__(self):
+        return "%s: %s" % (self.student.full_name, self.worksheet.name,)
+    
+class FullLengthTest(models.Model):
+    name = models.CharField(max_length=64)
+    course = models.ForeignKey(Course)
+
+    def __unicode__(self):
+        return "%s: %s" % (self.course.name, self.name,)
+
+
+class SectionalTest(models.Model):
+    name = models.CharField(max_length=128)
+    section = models.ForeignKey(CourseSection)
+
+    def __unicode__(self):
+        return "%s: %s: %s" % (self.section.course.name, self.section.name, self.name,)
+
+        
+class StudentFullLengthTest(models.Model):
+    student = models.ForeignKey(Student)
+    test = models.ForeignKey(FullLengthTest)    
+    scores = models.ManyToManyField(CourseSection, through="StudentTestSectionalScore")
+#    score_total = models.IntegerField(null=True)
+    notes = models.TextField(blank=True)
+    date = models.DateField()
+
+    def __unicode__(self):
+        return "%s: %s" % (self.student.full_name, self.test.name,)
+
+class StudentTestSectionalScore(models.Model):
+    student_flt = models.ForeignKey(StudentFullLengthTest)
+    section = models.ForeignKey("CourseSection")
+    score = models.IntegerField()
+    
+
+class StudentSectionalTest(models.Model):
+    student = models.ForeignKey(Student)
+    sectional_test = models.ForeignKey(SectionalTest)
+    score = models.IntegerField()
+    date = models.DateField()
+
+    def __unicode__(self):
+        return "%s: %s" % (self.student.full_name, self.sectional_test.name,)
+#    
+
+
+class Exam(models.Model):
+    date = models.DateField()
+    course = models.ForeignKey(Course)
+
+    def __unicode__(self):
+        dtstring = self.date.strftime("%B, %Y")
+        return "%s: %s" % (self.course.name, dtstring,)
+
+
+class StudentExam(models.Model):
+    student = models.ForeignKey(Student)
+    exam = models.ForeignKey(Exam)
+    scores = models.ManyToManyField(CourseSection, through="StudentExamSectionScore")
+#    score_breakup = DictField(default={})
+    total_score = models.IntegerField(null=True)
+    skipped = models.BooleanField(default=False)
+    taken = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return "%s: %s" % (self.student.full_name, self.exam,)
+
+
+class StudentExamSectionScore(models.Model):
+    student_exam = models.ForeignKey(StudentExam)
+    section = models.ForeignKey(CourseSection)
+    score = models.IntegerField()
+
+
+class StudentCourse(models.Model):
+    student = models.ForeignKey(Student)
+    course = models.ForeignKey(Course)
+    start_date = models.DateField()
+    end_date = models.DateField(null=True)
+    
+    def __unicode__(self):
+        return "%s: %s" % (self.student.full_name, self.course.name,)
+
+WEEKDAY_CHOICES = (
+    (0, 'Sunday'),
+    (1, 'Monday'),
+    (2, 'Tuesday'),
+    (3, 'Wednesday'),
+    (4, 'Thursday'),
+    (5, 'Friday'),
+    (6, 'Saturday'),
+)
+
+class Batch(models.Model):
+    centre = models.ForeignKey("Centre")
+    module = models.ForeignKey("CourseModule")
+    day = models.IntegerField(choices=WEEKDAY_CHOICES)
+    time = models.TimeField()
+    tutor  = models.ForeignKey("Tutor")
+
+    class Meta:
+        verbose_name_plural = "Batches"
+
+    def __unicode__(self):
+        day = WEEKDAY_CHOICES[self.day][1]
+        return " %s at %s on %ss" % (self.module, self.centre, day,)    
+
+class Attendance(models.Model):
+    batch = models.ForeignKey(Batch)
+    datetime = models.DateTimeField()
+    tutor = models.ForeignKey(Tutor)
+    students = models.ManyToManyField(Student)
+
+    def __unicode__(self):
+        return unicode(self.batch) + " by " + unicode(self.tutor)
+
+class FeedbackSession(models.Model):
+    tutor = models.ForeignKey("Tutor")
+    start_time = models.DateTimeField()
+    duration = models.IntegerField(null=True, help_text="in hours")
+    students = models.ManyToManyField("Student")
+
+    def __unicode__(self):
+        dtstring = self.start_time.strftime("%d %b, %Y at %H:%M")
+        return "%s: %s" % (self.tutor, dtstring,)
