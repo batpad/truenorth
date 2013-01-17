065b37f7977a93095f8c50fd2403eb3fa06495da
satyag <satyag@satyag-Lenovo-IdeaPad-S300.(none)>
2013-01-17 18:42:08 +0530

    adding the model to add users to admin

--- sat/login/admin.py
+++ sat/login/admin.py
@@ -1,22 +1,64 @@
-from django import forms
 from django.contrib import admin
-from django.contrib.auth.models import Group
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.conf import settings
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+ 
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+ 
+        user = self.model(
+            email=MyUserManager.normalize_email(email),
+        )
+ 
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+ 
+    def create_superuser(self, email, password):
+        user = self.create_user(email,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+ 
+ 
+class MyUser(AbstractBaseUser):
+    email = models.EmailField(max_length=254, unique=True, db_index=True)
+ 
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+ 
+    objects = MyUserManager()
+ 
+    USERNAME_FIELD = 'email'
+ 
+    def get_full_name(self):
+        # For this case we return email. Could also be User.first_name User.last_name if you have these fields
+        return self.email
+ 
+    def get_short_name(self):
+        # For this case we return email. Could also be User.first_name if you have this field
+        return self.email
+ 
+    def __unicode__(self):
+        return self.email
+ 
+    def has_perm(self, perm, obj=None):
+        # Handle whether the user has a specific permission?"
+        return True
+ 
+    def has_module_perms(self, app_label):
+        # Handle whether the user has permissions to view the app `app_label`?"
+        return True
+ 
+    @property
+    def is_staff(self):
+        # Handle whether the user is a member of staff?"
+        return self.is_admin
+ 
 
-from sat.login import *
-
-
-class UserProfileAdmin(admin.ModelAdmin):
-    date_hierarchy = 'created_on'
-    list_display = ('id', 'username', 'full_name', 'email', 'location',
-        'preflang', 'featured', 'created_on')
-    list_filter = list_display[5:]
-    search_fields = list_display[:5]
-
-
-class TaggedProfileAdmin(admin.ModelAdmin):
-    list_display = ('id', 'tag')
-
-#admin.site.register(login, UserProfileAdmin)
-#admin.site.register(TaggedProfile, TaggedProfileAdmin)
+admin.site.register( MyUser )
+#admin.site.register( MyUserManager)
