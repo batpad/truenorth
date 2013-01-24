73a1f7a0e90c3481411fbbde0456dbc36038d4b7
Satyaakam <satyaakam@gmail.com>
2013-01-25 01:19:52 +0530

    password reset

--- sat/login/urls.py
+++ sat/login/urls.py
@@ -3,5 +3,16 @@ from views import login_user
 
 
 urlpatterns = patterns('',
+url(r'^login/password/reset/$', 
+        'django.contrib.auth.views.password_reset', 
+        {'post_reset_redirect' : '/user/password/reset/done/'},
+        name="password_reset"),
+    (r'^login/password/reset/done/$',
+        'django.contrib.auth.views.password_reset_done'),
+    (r'^login/password/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 
+        'django.contrib.auth.views.password_reset_confirm', 
+        {'post_reset_redirect' : '/user/password/done/'}),
+    (r'^login/password/done/$', 
+        'django.contrib.auth.views.password_reset_complete'),
     (r'^login/$', login_user),
 )
